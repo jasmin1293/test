f5df7045490598e56e4565558e8d1cbf2a4935bd
Jame pack <darknet009-beep@ukr.net>
2026-07-23 23:37:34 +0300

    update

--- index.js
+++ index.js
@@ -4,16 +4,24 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+function isBrowserUserAgent(userAgent) {
+  return /Mozilla\/5\.0|Chrome|Firefox|Safari|Edge/i.test(userAgent);
+}
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/*', (req, res) => {
+const userAgent = req.get('User-Agent') || '';
 
+    if (isBrowserUserAgent(userAgent)) {
+      return res.type('text/plain').send('{\"error\":\"Not Found\"}');
+    } else {
   const filePath = path.join(__dirname, 'public', 'sample');
   res.type('text/plain').sendFile(filePath, (err) => {
     if (err) {
       res.status(404).json({ error: '\"error\":\"Not Found\"}' });
     }
   });
+}
 });
 
 if (require.main === module) {
